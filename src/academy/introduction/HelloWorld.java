65470e9d9cd1e5f15b09ffd74444ab6c6bdb06f1
Samuel Ribas <samuelfribas@gmail.com>
2025-12-31 11:50:39 -0300

    Lesson 1 and 2

---
+++ src/academy/introduction/HelloWorld.java
@@ -0,0 +1,8 @@
+package academy.introduction;
+
+public class HelloWorld {
+    static void main() {
+        System.out.println("Hello World");
+    }
+}
+
---
+++ src/academy/introduction/PrimitiveDataTypes.java
@@ -0,0 +1,12 @@
+package academy.introduction;
+
+/**
+ * This is a <b>Javadoc</b> comment
+ */
+public class PrimitiveDataTypes {
+    public static void main(String[] args){
+        // int, double, float, char, byte, boolean, short, long
+        int age = 23;
+        System.out.println("My age is " +age+ " years old");
+    }
+}
