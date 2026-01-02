29080cbc83b2b90c2426a3368784f8cc97aa9062
Samuel Ribas <samuelfribas@gmail.com>
2026-01-02 19:02:18 -0300

    starting exercises

---
+++ src/academy/introduction/ExercicesPrimitivesTypes.java
@@ -0,0 +1,14 @@
+package academy.introduction;
+import java.util.Date;
+
+/**
+ * exercises
+ */
+public class ExercicesPrimitivesTypes {
+    static void main() {
+        int age = 23;
+        String name = "Samuel", address = "Broadway NY, 10012", date = "12/20/2025";
+        double wage = 2500.50D;
+        System.out.println("I, " +name+ ", resident of " +address+ ", confirm that i received a salary of US$" +wage+ " dollars, on " +date);
+    }
+}
