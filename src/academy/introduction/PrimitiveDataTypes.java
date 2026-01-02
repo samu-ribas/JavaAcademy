d53c735f392cdde335e162b422dfeb0055a7e8fb
Samuel Ribas <samuelfribas@gmail.com>
2026-01-02 14:05:13 -0300

    primitive data types

--- src/academy/introduction/PrimitiveDataTypes.java
+++ src/academy/introduction/PrimitiveDataTypes.java
@@ -7,6 +7,20 @@ public class PrimitiveDataTypes {
     public static void main(String[] args){
         // int, double, float, char, byte, boolean, short, long
         int age = 23;
+        long bigNumber = (long) 155.56;
+        double doubleNumber = 2000.31D;
+        float floatNumber = (float) 1.3D;
+        byte byteAge = 127;
+        short shortAge = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char character = '\u0041';
+        String name = "goku";
+
         System.out.println("My age is " +age+ " years old");
+        System.out.println(falso);
+        System.out.println("char " +character);
+        System.out.println(bigNumber);
+        System.out.println("Hi, my name is "+name);
     }
 }
